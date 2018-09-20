a1fe89f7a0fd8fc5d3251808510fa87289dc8c0f
vinipatil <vinayakagouda.p@testyantra.com>
2018-09-20 06:18:31 +0530

    commit 6

--- features/games/templates/classification/classification.py
+++ features/games/templates/classification/classification.py
@@ -10,7 +10,6 @@ class Classification(BaseClass):
             
     def drag_object_to_bucket(self, draggable_name, bucket_name):
         self.altdriver.wait_for_element_where_name_contains(draggable_name).tap()
-        self.altdriver.wait_for_element_where_name_contains(draggable_name).tap()
         self.altdriver.wait_for_element_where_name_contains(draggable_name).dragToElement(
             self.altdriver.wait_for_element_where_name_contains(bucket_name)
             )
