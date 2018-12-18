c73ac0fb4cf70f295cfee005ae7c6b69e8e8d723
vinipatil <vinayakagouda.p@testyantra.com>
2018-12-18 18:17:01 +0530

    changes in feature files

--- features/execution.py
+++ features/execution.py
@@ -19,11 +19,11 @@ def start_execution(feature_file=None):
         
     if feature_file == None:
         subprocess.Popen(
-            'behave -D APP_PATH=C:\\Users\\Administrator.TNPLIND0007\\Downloads\\k123_app.apk -D DEVICE_TYPE=android -D MACHINE_TYPE=windows -D TESTRAIL_USER=archana.r@testyantra.com -D TESTRAIL_PASS=Pass1234',
+            'behave -D APP_PATH=sys.argv[4] -D DEVICE_TYPE=android -D MACHINE_TYPE=windows -D TESTRAIL_USER=archana.r@testyantra.com -D TESTRAIL_PASS=Pass1234',
             shell=False)
     else:
         subprocess.Popen(
-            'behave -D APP_PATH=C:\\Users\\Administrator.TNPLIND0007\\Downloads\\k123_app.apk -D DEVICE_TYPE=android -D MACHINE_TYPE=windows -D TESTRAIL_USER=archana.r@testyantra.com -D TESTRAIL_PASS=Pass1234 '+feature_file,
+            'behave -D APP_PATH=sys.argv[4] -D DEVICE_TYPE=android -D MACHINE_TYPE=windows -D TESTRAIL_USER=archana.r@testyantra.com -D TESTRAIL_PASS=Pass1234 '+feature_file,
             shell=False)
 # -f allure_behave.formatter:AllureFormatter -o ../execution_data/reports 
 
